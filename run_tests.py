fa6db3365a555f1c3947e2bacf106fdac62d5895
petrukhin_a <andrey.petrukhin@kaspersky.com>
2022-05-21 15:58:53 +0300

    finale result

---
+++ run_tests.py
@@ -0,0 +1,118 @@
+#!/usr/bin/python3
+
+from time import sleep
+import unittest as ut
+import os as os
+
+device_filename   = "/dev/simple_serial_device"
+module_name       = "simple_serial_device"
+module_filename   = "{0}.ko".format(module_name)
+module_parameters = "buffer_size=32"
+
+class Test(ut.TestCase):
+	def __load_module__(self):
+		res = os.system("insmod {0} {1}".format(module_filename, module_parameters));
+		self.assertEqual(res, 0, "Cannot load module")
+	
+	def __unload_module__(self):
+		res = os.system("rmmod {0}".format(module_filename));
+		self.assertEqual(res, 0, "Cannot unload module")
+
+	def __check_module_loaded__(self):
+		res = os.system("lsmod  | grep {0} > /dev/null".format(module_name));
+		self.assertEqual(res, 0, "Cannot check  module loaded")
+
+	def setUp(self):
+		self.__load_module__()
+
+	def tearDown(self):
+		self.__unload_module__()
+
+	####################### Tests ##############################
+	def test_load_unload(self):
+		self.__check_module_loaded__()
+
+	def test_open_device_file_read(self):
+		with  open(device_filename, "r") as dev_file:
+			pass
+
+	def test_open_device_file_write(self):
+		with  open(device_filename, "w") as dev_file:
+			pass
+
+	def test_open_device_file_append(self):
+		with  open(device_filename, "r+") as dev_file:
+			pass
+
+	def test_writeToEmpty(self):
+		write_str = "RnVjawo"
+		with  open(device_filename, "w") as dev_file:
+			res = dev_file.write(write_str)
+		self.assertEqual(len(write_str), res, "Cannot write {0} sybmol(s)".format(len(write_str)))
+
+	def test_readFromEmpty(self):
+		with  open(device_filename, "r") as dev_file:
+			read_buff = dev_file.read(1)
+		self.assertEqual(read_buff, "", "Read buffer not empty")
+
+	def test_writeRead(self):
+		write_str = "dGhpcwo"
+		with  open(device_filename, "r+") as dev_file:
+			res = dev_file.write(write_str)
+			self.assertEqual(len(write_str), res, "Cannot write {0} sybmol(s)".format(len(write_str)))
+
+			dev_file.seek(0)		# because file opened in non O_DIRECT mode
+
+			read_buff = dev_file.read(len(write_str))
+			self.assertEqual(read_buff, write_str, "Mismatch between reading and writing")
+
+
+	def test_writeReadWrite(self):
+		write_str = "ZXhhbXBsZQo"
+		with  open(device_filename, "r+") as dev_file:
+			res = dev_file.write(write_str)
+			self.assertEqual(len(write_str), res, "Cannot write {0} sybmol(s)".format(len(write_str)))
+
+			dev_file.seek(0)		# because file opened in non O_DIRECT mode
+
+			read_buff = dev_file.read(len(write_str))
+			self.assertEqual(read_buff, write_str, "Mismatch between reading and writing")
+
+			res = dev_file.write(write_str)
+			self.assertEqual(len(write_str), res, "Cannot write {0} sybmol(s)".format(len(write_str)))
+
+	def test_writeUntillError(self):
+		write_str = "YWdhaW4K"
+		with  open(device_filename, "r+") as dev_file:
+			try:
+				for _ in range(0, 8196):
+					res = dev_file.write(write_str)
+			except IOError:
+				pass	# if test passed, we should be there
+			else:
+				self.fail("Operation write should not be permitted")
+
+	def test_readUntillError(self):
+		write_str = "YW5kIGFnYWluCg"
+		with  open(device_filename, "r+") as dev_file:
+			res = dev_file.write(write_str)
+			self.assertEqual(len(write_str), res, "Cannot write {0} sybmol(s)".format(len(write_str)))
+
+			dev_file.seek(0)		# because file opened in non O_DIRECT mode
+
+			dev_file.read()
+			buf = dev_file.read()
+			self.assertEqual(buf, "")
+
+	def test_openWriteCloseOpenRead(self):
+		write_str = "YW5kIGFnYWluCg"
+		with  open(device_filename, "w") as dev_file:
+			res = dev_file.write(write_str)
+			self.assertEqual(len(write_str), res, "Cannot write {0} sybmol(s)".format(len(write_str)))
+
+		with  open(device_filename, "r") as dev_file:
+			buf = dev_file.read()
+			self.assertEqual(buf, write_str, "Mismatch between reading and writing")
+
+if __name__ == '__main__':
+	ut.main()
---
+++ simple_serial_device.c
@@ -0,0 +1,278 @@
+#include <linux/cdev.h>
+#include <linux/fs.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/slab.h>
+#include <linux/uaccess.h>
+
+#include <linux/circ_buf.h>
+
+
+#define SIMPLE_SERIAL_DEVICE_CREATE_FILE
+
+#define DRIVER_NAME    "my_device_driver"
+
+static int buffer_size=256;
+module_param(buffer_size, int, 0);
+
+#ifdef SIMPLE_SERIAL_DEVICE_CREATE_FILE
+
+#define CH_FILE_NAME   "simple_serial_device"
+#define CLASS_DEVICE   "simple_serial_device_class"
+
+#endif
+
+static DEFINE_SPINLOCK(consumer_lock);
+static DEFINE_SPINLOCK(producer_lock);
+
+struct my_circ_buffer {
+	char* 		buffer;
+	unsigned long head;
+	unsigned long tail;
+	ssize_t 	  size;
+};
+
+#ifdef SIMPLE_SERIAL_DEVICE_CREATE_FILE
+	static struct class *dev_class;
+	static struct device *device;
+#endif
+
+static struct cdev my_cdev;
+static struct my_circ_buffer  circ_buffer;
+static unsigned int my_major                = 0;
+static unsigned int num_of_dev              = 1;
+
+
+static int __is_pow_2(int val)
+{
+	int count = 0;	
+	while(val)
+	{
+		count += (val & 1);
+		val >>=1;
+	}
+	return count == 1;
+}
+
+static int my_open(struct inode *inode, struct file *file)
+{
+	file->private_data      = &circ_buffer;
+	return 0;
+}
+
+static loff_t my_seek(struct file *file, loff_t offset, int whence)
+{
+	// this stumb make the device file 'seekable', we don't use it
+	return 0;
+}
+
+static ssize_t my_write(struct file *file, const char __user *user_buffer,
+					size_t size, loff_t * offset)
+{
+	struct my_circ_buffer* p_circ_buffer;
+	ssize_t ret = -1;
+	unsigned long head;
+	unsigned long tail;
+	ssize_t circ_space = 0;
+	spin_lock(&producer_lock);
+
+	p_circ_buffer = (struct my_circ_buffer*)file->private_data;
+	head = p_circ_buffer->head;
+	tail = READ_ONCE(p_circ_buffer->tail);
+
+	circ_space = CIRC_SPACE(head, tail, p_circ_buffer->size);
+
+	if (circ_space >= 1)
+	{
+
+			char* p_item  = &(p_circ_buffer->buffer[head]);
+
+			if (copy_from_user(p_item, user_buffer, 1))
+			{
+				ret = -EFAULT;
+				goto out;
+			}
+			smp_store_release(&(p_circ_buffer->head),
+							(head + 1) & (p_circ_buffer->size - 1));
+			ret = 1;
+	}
+out:
+	spin_unlock(&producer_lock);
+	return ret;
+}
+
+static ssize_t my_read(struct file *file, char *user_buffer, size_t size, loff_t *offset)
+{
+	struct my_circ_buffer* p_circ_buffer;
+	ssize_t ret = -1;
+	unsigned long head;
+	unsigned long tail;
+	ssize_t circ_cnt = 0;
+
+	spin_lock(&consumer_lock);
+	p_circ_buffer = (struct my_circ_buffer*)file->private_data;
+	head = smp_load_acquire(&(p_circ_buffer->head));
+	tail = p_circ_buffer->tail;
+
+	circ_cnt = CIRC_CNT(head, tail, p_circ_buffer->size);
+
+	if (circ_cnt >= 1)
+	{
+			char* item = &(p_circ_buffer->buffer[tail]);
+
+			if(copy_to_user(user_buffer, item, 1))
+			{
+				pr_err("%s: %d copy_to_user failed\n", __func__, __LINE__);
+				ret = -EFAULT;
+				goto out;
+			}
+
+			smp_store_release(&(p_circ_buffer->tail),
+							(tail + 1) & (p_circ_buffer->size - 1));
+		ret =  1;
+	}
+	else
+	{
+		ret = 0;
+	}
+
+ out:
+	spin_unlock(&consumer_lock);
+
+	return ret;
+}
+
+static int my_release(struct inode* __inode, struct file* __file)
+{
+	pr_info("release serial device\n");
+	return 0;
+}
+
+const struct file_operations my_fops = {
+	.owner   = THIS_MODULE,
+	.open    = my_open,
+	.llseek  = my_seek,
+	.read    = my_read,
+	.write   = my_write,
+	.release = my_release,
+};
+
+static int my_init(void)
+{
+	dev_t device_id;
+	int alloc_err      = -1;
+	int add_err        = -1;
+	int enomem	       = -1;
+
+#ifdef SIMPLE_SERIAL_DEVICE_CREATE_FILE
+	int class_cr_err   = -1;
+	int dev_cr_err     = -1;
+#endif
+
+	if(!__is_pow_2(buffer_size) && buffer_size > 1)
+	{
+		pr_err("buffer_size=%d have to be pow2\n", buffer_size);
+		goto error_out;
+	}
+	alloc_err= alloc_chrdev_region(&device_id, 0, num_of_dev, DRIVER_NAME);
+
+	if(alloc_err != 0)
+	{
+		pr_err("register device error %d\n", alloc_err);
+		goto error_out;
+	}
+
+	my_major = MAJOR(device_id);
+
+	cdev_init(&my_cdev, &my_fops);
+
+	add_err = cdev_add(&my_cdev, device_id, num_of_dev);
+
+	if(add_err != 0)
+	{
+		pr_err("add device error %d\n", add_err);
+		goto error_out;
+	}
+
+#ifdef SIMPLE_SERIAL_DEVICE_CREATE_FILE
+	dev_class = class_create(THIS_MODULE, CLASS_DEVICE);
+	if(dev_class == NULL)
+	{
+		pr_err("Cannot create the struct class %s for device\n", CLASS_DEVICE);
+		goto error_out;
+	}
+	class_cr_err = 0;
+
+	device = device_create(dev_class,NULL,device_id,NULL, CH_FILE_NAME);
+	if(device == NULL)
+	{
+		pr_err("Cannot create the device file %s\n", CH_FILE_NAME);
+		goto error_out;
+	}
+	dev_cr_err = 0;
+#endif
+
+	circ_buffer.buffer = kmalloc(buffer_size, GFP_KERNEL);
+
+	if(circ_buffer.buffer == NULL)
+	{
+		goto error_out;
+	}
+	enomem = 0;
+	circ_buffer.head = 0;
+	circ_buffer.tail = 0;
+	circ_buffer.size = buffer_size;
+
+	pr_info("%s driver(major: %d) installed.\n", DRIVER_NAME, my_major);
+
+	return 0;
+
+error_out:
+	if(add_err == 0)
+	{
+		cdev_del(&my_cdev);
+	}
+
+	if(alloc_err == 0)
+	{
+		unregister_chrdev_region(device_id, num_of_dev);
+	}
+	
+#ifdef SIMPLE_SERIAL_DEVICE_CREATE_FILE
+	if(class_cr_err == 0)
+	{
+        class_destroy(dev_class);
+	}
+
+	if(dev_cr_err == 0)
+	{
+        unregister_chrdev_region(device_id,1);
+	}
+#endif
+
+	if(enomem == 0)
+	{
+		kfree(circ_buffer.buffer);
+	}
+	return -1;
+}
+
+static void my_exit(void)
+{
+	dev_t dev = MKDEV(my_major, 0);
+
+	cdev_del(&my_cdev);
+
+#ifdef SIMPLE_SERIAL_DEVICE_CREATE_FILE
+	device_destroy(dev_class,dev);
+	class_destroy(dev_class);
+#endif
+
+	unregister_chrdev_region(dev, num_of_dev);
+	pr_info("%s driver removed\n", DRIVER_NAME);
+	kfree(circ_buffer.buffer);
+}
+
+module_init(my_init)
+module_exit(my_exit)
+MODULE_LICENSE("GPL");
